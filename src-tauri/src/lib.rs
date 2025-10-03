f53d03d6d6bd4cb19204cacea7a1f00909ec2634
nerdyfitzy <65741665+nerdyfitzy@users.noreply.github.com>
2025-10-03 00:10:30 -0400

    v0.2.0 (FINAL)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -179,40 +179,43 @@ fn create_vods_dir() {
     }
 }
 
-async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
-    if let Some(update) = app.updater()?.check().await? {
-        let mut downloaded = 0;
-
-        // alternatively we could also call update.download() and update.install() separately
-        update
-            .download_and_install(
-                |chunk_length, content_length| {
-                    downloaded += chunk_length;
-                    println!("downloaded {downloaded} from {content_length:?}");
-                },
-                || {
-                    println!("download finished");
-                },
-            )
-            .await?;
-
-        println!("update installed");
-        app.restart();
-    }
-
-    Ok(())
-}
+// async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+//     if let Some(update) = app.updater()?.check().await? {
+//         let mut downloaded = 0;
+//
+//         // alternatively we could also call update.download() and update.install() separately
+//         update
+//             .download_and_install(
+//                 |chunk_length, content_length| {
+//                     downloaded += chunk_length;
+//                     println!("downloaded {downloaded} from {content_length:?}");
+//                 },
+//                 || {
+//                     println!("download finished");
+//                 },
+//             )
+//             .await?;
+//
+//         println!("update installed");
+//         app.restart();
+//     }
+//
+//     Ok(())
+// }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     create_vods_dir();
 
-    Builder::default()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
-            let handle = app.handle().clone();
-            tauri::async_runtime::spawn(async move {
-                update(handle).await.unwrap();
-            });
+            // let handle = app.handle().clone();
+            // tauri::async_runtime::spawn(async move {
+            //     update(handle).await.unwrap();
+            // });
             app.manage(Mutex::new(AppState::default()));
             Ok(())
         })
